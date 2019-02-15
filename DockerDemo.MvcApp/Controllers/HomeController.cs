d28cfe40ed779c30a43e4b9215c968d8855e75d6
Kapil Khadgi <kapil.khadgi@perficient.com>
2019-02-15 17:25:23 +0530

    working first example

---
+++ DockerDemo.MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DockerDemo.MvcApp.Models;
+using System.Net.Http;
+
+namespace DockerDemo.MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<string> Index()
+        {
+            HttpClient client = new HttpClient();
+
+            string path = "http://api/api/values";
+            string product = null;
+
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsStringAsync();
+            }
+
+            return $"Values coming from API are : {product}";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
